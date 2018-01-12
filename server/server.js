9cad4ab5bde8a4c5091388760e162f3fb588d2ec
Sudhin <sudhinmachad@gmail.com>
2018-01-12 14:54:49 +0530

    Server created,Registered user can only login to the page

---
+++ server/server.js
@@ -0,0 +1,133 @@
+let fs = require('fs');
+const timeStamp = require('./time.js').timeStamp;
+const http = require('http');
+const WebApp = require('./webapp');
+
+let registered_users = [
+  {userName:'sudhin',name:'Sudhin MN',password:'123'},
+  {userName:'sreenadh',name:'Sreenadh S',password:'123'}];
+
+let toStr = obj=>JSON.stringify(obj,null,2);
+
+let logRequest = (req,res)=>{
+  let text = ['------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${toStr(req.headers)}`,
+    `COOKIES=> ${toStr(req.cookies)}`,
+    `BODY=> ${toStr(req.body)}`,''].join('\n');
+  fs.appendFile('request.log',text,()=>{});
+
+  console.log(`${req.method} ${req.url}`);
+}
+
+let loadUser = (req,res)=>{
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u=>u.sessionid==sessionid);
+  if(sessionid && user){
+    req.user = user;
+  }
+};
+
+let serveStaticFiles=function(req,res){
+  let fileUrl='../public'+req.url;
+  if(fs.existsSync(fileUrl)){
+    try{
+      let fileContent=getFileContent(fileUrl,null);
+      setContentType(fileUrl,res);
+      res.write(fileContent);
+      res.end();
+    }catch(ex){}
+  }
+};
+
+const getContentType=function(extension){
+  let contentType={
+    '.html':'text/html',
+    '.js':'text/js',
+    '.css':'text/css',
+    '.jpeg':'image/jpeg',
+    '.jpg':'image/jpg',
+    '.gif':'image/gif',
+    '.pdf':'application/pdf',
+  }
+  return contentType[extension];
+};
+
+const setContentType=function(fileUrl,res){
+  let extension=fileUrl.slice(fileUrl.lastIndexOf('.'));
+  let contentType=getContentType(extension);
+  res.setHeader('content-type',contentType);
+};
+
+const getFileContent=function(file,encoding='utf8'){
+  return fs.readFileSync(file,encoding);
+};
+
+const writeToFile=function(content,filename){
+  let existingData=getFileContent(filename);
+  let newData=JSON.parse(existingData);
+  newData.unshift(content);
+  fs.writeFileSync(filename,JSON.stringify(newData,null,2));
+};
+
+
+const parseToHTML=function(commentsList){
+  let content="";
+  commentsList.forEach( (comment)=>{
+    content+=`<pre>${comment.date} ${comment.name} ${comment.comment}</pre>`
+  });
+  return content;
+};
+
+const getLoginPage=function(req,res){
+  let fileContent=getFileContent('../public/login.html');
+  res.setHeader('Content-type','text/html');
+  if(req.cookies.logInFailed){
+    // res.write('Login Failed');
+  }
+  res.write(fileContent);
+  res.end();
+};
+
+const getUserData=function(req){
+  console.log(req.body);
+  return registered_users.find(u=>u.userName==req.body.userName&&u.password==req.body.pwd);
+};
+
+const validatePostUserData=function(req,res){
+  let user = getUserData(req);
+  if(!user) {
+    res.setHeader('Set-Cookie',[`logInFailed=true`,'Max-Age=5']);
+    res.redirect('/login.html');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/index.html');
+};
+
+const logoutUser=function(req,res){
+  console.log("Entering");
+  res.setHeader('Set-Cookie',[`loginFailed=false,Expires=${new Date(1).toUTCString()}`,`sessionid=0,Expires=${new Date(1).toUTCString()}`]);
+  delete req.user.sessionid || {};
+  res.redirect('/login.html');
+}
+
+let app = WebApp.create();
+
+app.use(logRequest);
+app.use(loadUser);
+
+app.get('/',(req,res)=>{res.redirect('login.html')});
+app.get('/login.html',getLoginPage);
+app.post('/login.html',validatePostUserData);
+app.get('/logout',logoutUser);
+
+app.postprocess(serveStaticFiles);
+
+const PORT = 9000;
+let server = http.createServer(app);
+server.on('error',e=>console.error('**error**',e.message));
+server.listen(PORT,(e)=>console.log(`server listening at ${PORT}`));
---
+++ server/webapp.js
@@ -0,0 +1,111 @@
+
+let redirect = function(path){
+  console.log(`redirecting to ${path}`);
+  this.statusCode = 302;
+  this.setHeader('location',path);
+  this.end();
+};
+
+let invoke = function(req,res){
+  let handler = this._handlers[req.method][req.url];
+  if(!handler){
+    return;
+  }
+  handler(req,res);
+};
+
+let urlIsOneOf = function(urls){
+  return urls.includes(this.url);
+}
+
+const toKeyValue = kv=>{
+    let parts = kv.split('=');
+    return {key:parts[0].trim(),value:parts[1].trim()};
+};
+
+const accumulate = (o,kv)=> {
+  o[kv.key] = kv.value;
+  return o;
+};
+
+const parseBody = text=> text && text.split('&').map(toKeyValue).reduce(accumulate,{}) || {};
+
+const parseCookies = text=> {
+  try {
+    return text && text.split(';').map(toKeyValue).reduce(accumulate,{}) || {};
+  }catch(e){
+    return {};
+  }
+}
+
+const fileNotFoundAction=function(res){
+  res.statusCode = 404;
+  res.write('File not found!');
+  res.end();
+};
+
+const initialize = function(){
+  this._handlers = {GET:{},POST:{}};
+  this._preprocess = [];
+  this._postprocess= [];
+};
+
+const get = function(url,handler){
+  this._handlers.GET[url] = handler;
+};
+
+const post = function(url,handler){
+  this._handlers.POST[url] = handler;
+};
+
+const use = function(handler){
+  this._preprocess.push(handler);
+};
+
+const postprocess=function(handler){
+  this._postprocess.push(handler);
+};
+
+const main = function(req,res){
+  // console.log(req.headers);
+  res.redirect = redirect.bind(res);
+  req.urlIsOneOf = urlIsOneOf.bind(req);
+  req.cookies = parseCookies(req.headers.cookie||'');
+  let content="";
+  req.on('data',data=>content+=data.toString())
+  req.on('end',()=>{
+    req.body = parseBody(content);
+    content="";
+    debugger;
+
+    this._preprocess.forEach(middleware=>{
+      if(res.finished) return;
+      middleware(req,res);
+    });
+
+    if(res.finished) return;
+    invoke.call(this,req,res);
+
+    this._postprocess.forEach(finalware=>{
+      if(res.finished) return;
+      finalware(req,res);
+    });
+    if(res.finished)return;
+    res.writeHead(404,{'content-type':'text/html'});
+    res.write('<h1>Error:404 Resource Not Found !</h1>');
+    res.end()
+  });
+};
+
+let create = ()=>{
+  let rh = (req,res)=>{
+    main.call(rh,req,res)
+  };
+  initialize.call(rh);
+  rh.get = get;
+  rh.post = post;
+  rh.use = use;
+  rh.postprocess=postprocess;
+  return rh;
+}
+exports.create = create;
